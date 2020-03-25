82e51c056a1ca1ef677dde930bef70c29d4657cd
peter gilkey <peter_gilkey@yahoo.com>
2020-03-24 20:22:43 -0500

    Datatypes assignment

---
+++ 01week/datatypes.js
@@ -0,0 +1,18 @@
+'use strict'
+
+//Wite a JavaScript program to display the current date and time.
+let now = new Date();
+console.log("The current date and time is ", now);
+
+//Write a JavaScript program to convert a number to a string.
+const theNumber = 5;
+const theString = theNumber.toString();
+
+console.log("The type of theString variable is ", (typeof theString))
+console.log("The string is ", theNumber.toString())
+console.log("The number is ", theNumber);
+
+//Write a JavaScript program to convert a string to a number
+const theOtherString = "4.5";
+const theOtherNumber = parseInt(theOtherString, 10);
+console.log("The otherString type is ", (typeof theOtherString));
